a8a07eb0b5de23a834473c2fee70fe21fc85cc25
Mathias Klippinge <mathias.klippinge@gmail.com>
2017-11-09 09:52:45 +0100

    More updates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # phobosjs-mock
 
-Some simple ideas in code how we could be using phobos-js in practice.
+Some simple ideas in code how we could be using phobos in practice.
 
 The core ideas are
 - No multiprocessing, keep it simple.
@@ -8,15 +8,16 @@ The core ideas are
   - This alone should greatly reduce complexity of the Phobos code and make it easier to understand/contribute
   - Instead, scale horizontally
   - One service instance <=> one consumer <=> one configuration
-- Avoid metaprogramming and implicit contracts between users' services and phobos-js
+- Avoid meta programming and implicit contracts between users' services and phobos
   - The user is in control
   - The code written in services should be simple and readable, even if you don't know kafka (or even phobos!) you should still be able to understand
-- Move as much config as possible to `phobos.json`
+- Move as much config as possible to `.phobosjs`
 - Require as little config as possible inside the users' services, if any.
-- Think just as much about helping testing the users' services as writing them in the first place (similar to mappersmith)
-- [Possibility to decode avro encoded kafka messages](standaloneConsumer-serializer.js)
-- [Possiblility of wrapping the batch/message consumption and dealing with errors/success](standaloneConsumer-checkpoint.js)
-- Consider possibility of plugin support such as
+- Consider possibility of support such as
+  - Providing test tools for writing the consumer applications
+  - [Possibility to decode avro encoded kafka messages](standaloneConsumer-serializer.js)
+  - [Possibility of wrapping the message consumption and dealing with results (errors/success)](standaloneConsumer-messageWrapper.js)
   - phobos-checkpoint-ui
-  - phobos-prometheus (provide collectors and exporters for internal consumer/producer metrics)
-  - phobos-grafana (provide some rudimentary graphing of the data exported above)
+  - phobos-sensor
+    - => phobos-prometheus (provide collectors and exporters for internal consumer/producer metrics)
+    - => phobos-grafana (provide some rudimentary graphing of the data exported above)
--- standaloneConsumer-messageWrapper.js
+++ standaloneConsumer-messageWrapper.js
@@ -4,7 +4,7 @@
  * This app consumes the statement_available kafka topic and persists it
  * to the database as a Statement.
  *
- * The idea of a "checkpoint" is to provide a way to wrap the consumption of a message
+ * The idea of a "messageWrapper" is to provide a way to wrap the consumption of a message
  * around a try/catch and allowing for handling errors, retries, and even success results
  * in a configurable manner.
  *
@@ -16,31 +16,39 @@ const { createStatement } = require('./statement')
 /**
  * Instantiate a database checkPoint handler with config from .phobosdbcheckpointjs
  *
- * checkPoint implements an #messageWrapper closure to wrap the consumption of a message
- * which eventually invokes onMessage
+ * checkPoint implements a closure to wrap the consumption of a message which
+ * eventually invokes onMessage
  *
  */
 const checkPoint = createDBCheckpoint()
 
-const app = createConsumer({ topic: 'statement_available' })
-app.registerCheckpoint(checkPoint)
+/**
+ * Option 1
+ */
+const app = createConsumer({ topic: 'statement_available', messageWrapper: checkPoint })
 
 /**
- * If the callback is successful, the message will be persisted in checkPoint as
- * an event, if it throws it will be persisted in checkPoint as a failure (after retrying a few times)
+ * Option 2
+ */
+const app = createConsumer({ topic: 'statement_available' })
+app.registerMessageWrapper(checkPoint)
+
+/**
+ * If the callback is successful, the message will be persisted in db as
+ * an event, if it throws it will be persisted in db as a failure (after retrying a few times)
  */
 app.onMessage(async (message, metadata) => {
   const statement = createStatement(message.data())
 })
 
 /**
- * Considering that checkPoint will in some way handle errors...
+ * Considering that a messageWrapper will in some way handle errors...
  * Need to think about what happens with overlaps between checkPoint and regular phobos onError callbacks?
  * - Maybe don't need to handle this here, since we might be at risk of messing with checkPoint...?
  * - Or this is a "safe" callback that will always be invoked regardless of checkPoint?
  */
 app.onError(async error => {
-  console.log('Error happened, deal with it', error)
+  console.log('Error happened, but the checkPoint probably dealt with it', error)
 })
 
 app.run()
